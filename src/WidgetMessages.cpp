46fdd4c370cf14baea0731a3926260e6aa0b9a11
ArthurLlew <kiselyovei@yandex.ru>
2026-05-10 02:10:22 +0300

    Removed redundant code

--- src/WidgetMessages.cpp
+++ src/WidgetMessages.cpp
@@ -14,11 +14,4 @@ OSBMessageBox::OSBMessageBox(QString message) : QMessageBox()
 {
     // Set message and icon
     setText(message);
-
-    #ifdef Q_OS_WIN
-    // Set darkmode for Windows
-    BOOL use_dark_mode = true;
-    DwmSetWindowAttribute(HWND(winId()), DWMWINDOWATTRIBUTE::DWMWA_USE_IMMERSIVE_DARK_MODE,
-                            &use_dark_mode, sizeof(use_dark_mode));
-    #endif
 }

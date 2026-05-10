af0a2de9ea6a03ec1621cb9a25edbc415fb50d5c
ArthurLlew <kiselyovei@yandex.ru>
2026-05-10 21:32:16 +0300

    Added splash image on app startup

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include "embedded.h"
 // Main window widget
 #include <MainWindow.hpp>
+#include <QtWidgets/QSplashScreen>
 
 
 int main(int argc, char *argv[])
 {
     // Create application instance
     QApplication app(argc, argv);
+    // Show splash image while application is loading
+    QPixmap pixmap(":/resources/app.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
 
     // Load style sheet
     QString styleSheet = QLatin1String(QByteArray::fromRawData((char*)style_qss, sizeof(style_qss)));
@@ -16,6 +21,8 @@ int main(int argc, char *argv[])
     // Create and show main window
     MainWindow window(&app);
     window.show();
+    // Hide splash image
+    splash.finish(&window);
 
     // Run application loop
     return app.exec();
